4fad8aa4efc14b0c892a242510ece2af6da590c0
Miguel De Trool <migueldetrool@gmail.com>
2023-10-15 16:12:27 +0200

    Change imgPath to own variable ImageScanner

--- server/image_scanner.py
+++ server/image_scanner.py
@@ -9,6 +9,7 @@ class ImageScanner:
   def scanImage(self):
     currTime = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
     img = Image.new("RGB", (32, 32), color="white")
-    img.save(self.path + currTime + ".png")
+    imgPath = self.path + currTime + ".png"
+    img.save(imgPath)
     # subprocess.run(["scanimage", "--format=png", "--resolution=300", "-x 210", "-y 210", f"--o={self.path}{currTime}.png"])
-    subprocess.run(["scanimage", "--format=png", "--resolution=300", "--mode=24bit Color", "--AutoDocumentSize=yes", f"--o={self.path}{currTime}.png"])
+    subprocess.run(["scanimage", "--format=png", "--resolution=300", "--mode=24bit Color", "--AutoDocumentSize=yes", f"--o={imgPath}"])
