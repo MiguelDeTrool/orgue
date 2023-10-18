a93f8faa3bef90d03e17cec5ed475838d2c010b3
Miguel De Trool <migueldetrool@gmail.com>
2023-10-18 15:45:27 +0200

    Move setup code to own module

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,13 +1,3 @@
-let canvasContainer = document.querySelector(".canvas-container");
+import { pageSetup } from "./pageSetup.js";
 
-let url = document.URL;
-fetch(`${url}image`, {
-  mode: "cors",
-})
-  .then((response) => {
-    return response.json();
-  })
-  .then((response) => {
-    imgPath = response.imgPath.slice(25);
-    canvasContainer.style.backgroundImage = `url(${imgPath})`;
-  });
+pageSetup.getJson();
---
+++ server/static/js/pageSetup.js
@@ -0,0 +1,19 @@
+export const pageSetup = (() => {
+  let canvasContainer = document.querySelector(".canvas-container");
+  let url = document.URL;
+
+  const getJson = () => {
+    fetch(`${url}image`, {
+      mode: "cors",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        let imgPath = response.imgPath.slice(25);
+        canvasContainer.style.backgroundImage = `url(${imgPath})`;
+      });
+  };
+
+  return { getJson };
+})();
