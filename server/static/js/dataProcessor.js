c654e23a1cd51b3bd05153226f4284e1f19f4bba
Miguel De Trool <migueldetrool@gmail.com>
2023-10-29 22:27:30 +0100

    Make line to length ratio clearer

--- server/static/js/dataProcessor.js
+++ server/static/js/dataProcessor.js
@@ -13,45 +13,49 @@ export const DataProcessor = (subscriberArray) => {
 
   const _prepareData = () => {
     let workData = [];
-    let sortedCoordinates = _rawData.coordinates.toSorted(
-      (currPoint, nextPoint) => {
+    let sortedCoordinates = _rawData.coordinates
+      .slice(0, sequenceLength + 1) // Keep best points, with one extra to have final point to draw last line
+      .toSorted((currPoint, nextPoint) => {
         if (currPoint[0] > nextPoint[0]) {
           return 1;
         } else {
           return -1;
         }
-      }
-    );
+      });
 
     for (let i = 0; i <= sequenceLength; i++) {
-      let currCoor = sortedCoordinates[i];
-      let nextCoor = sortedCoordinates[i + 1];
       let point = {};
+      let currCoor = sortedCoordinates[i];
 
       point.coordinates = [];
       point.coordinates.push(currCoor[0]);
       point.coordinates.push(currCoor[1]);
 
-      point.distance = Math.hypot(
-        currCoor[0] - nextCoor[0],
-        currCoor[1] - nextCoor[1]
-      );
+      // Check if it's the last point to not add distance and pitch
+      if (i != sequenceLength) {
+        let nextCoor = sortedCoordinates[i + 1];
 
-      let angle = Math.atan2(
-        nextCoor[1] - currCoor[1],
-        nextCoor[0] - currCoor[0]
-      );
+        point.distance = Math.hypot(
+          currCoor[0] - nextCoor[0],
+          currCoor[1] - nextCoor[1]
+        );
 
-      angle = angle * (180 / Math.PI);
+        let angle = Math.atan2(
+          nextCoor[1] - currCoor[1],
+          nextCoor[0] - currCoor[0]
+        );
 
-      if (angle < -90) {
-        angle += 360;
+        angle = angle * (180 / Math.PI);
+
+        if (angle < -90) {
+          angle += 360;
+        }
+        angle -= 90;
+        angle = Math.abs(angle);
+
+        // point.pitch = Math.round(angle / 3 + 24);
+        point.pitch = angle;
       }
-      angle -= 90;
-      angle = Math.abs(angle);
-
-      // point.pitch = Math.round(angle / 3 + 24);
-      point.pitch = angle;
 
       workData.push(point);
     }
