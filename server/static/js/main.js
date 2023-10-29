ed59e0d39aae3660098e98365686bf622a7494d7
Miguel De Trool <migueldetrool@gmail.com>
2023-10-29 21:43:55 +0100

    Create MidiOutput class but not working on linux

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -2,11 +2,15 @@ import { PageSetup } from "./pageSetup.js";
 import { DataProcessor } from "./dataProcessor.js";
 import { Clock } from "./clock.js";
 import { CanvasAnimator } from "./canvasAnimator.js";
+import { MidiOutput } from "./midiOutput.js";
 
+const midiOutput = MidiOutput();
 const canvasAnimator = CanvasAnimator();
-const clock = Clock([canvasAnimator]);
+const clock = Clock([canvasAnimator, midiOutput]);
 
-const dataProcessor = DataProcessor([clock, canvasAnimator]);
+const dataProcessor = DataProcessor([clock, canvasAnimator, midiOutput]);
 
 const pageSetup = PageSetup(dataProcessor);
 pageSetup.getJson();
+
+setTimeout(clock.startClock, 1000);
---
+++ server/static/js/midiOutput.js
@@ -0,0 +1,53 @@
+// For info on how to setup virtual midi driver
+// https://tldp.org/HOWTO/MIDI-HOWTO-10.html
+
+export const MidiOutput = () => {
+  let output;
+  let pitches;
+  const onMIDISuccess = (midiAccess) => {
+    console.log("MIDI ready!");
+    console.log(midiAccess);
+    output = midiAccess.outputs.get(
+      "6FF5590044F4859ED50C5167BCFE9700A1798E39AA55A628E86D39011FAECD5D"
+    ); // Hard coded ALSA midi output ID
+    console.log(output);
+  };
+
+  const onMIDIFailure = (msg) => {
+    console.error(`Failed to get MIDI access - ${msg}`);
+  };
+
+  const update = (pointsData) => {
+    pitches = [];
+    pointsData.forEach((point) => {
+      pitches.push(point.pitch);
+    });
+  };
+
+  const tick = (noteIndex, fractionalDuration) => {
+    if ((fractionalDuration = 0)) {
+      for (let i = 0; i <= 127; i++) {
+        output.send([0x80, 60, 0]); // timestamp = now + 1000ms.
+      }
+      output.send([0x90, 60, pitches[noteIndex]]);
+    }
+  };
+
+  navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+
+  navigator.permissions.query({ name: "midi", sysex: true }).then((result) => {
+    if (result.state === "granted") {
+      // Access granted.
+    } else if (result.state === "prompt") {
+      // Using API will prompt for permission
+    }
+    // Permission was denied by user prompt or permission policy
+  });
+
+  return {
+    update,
+    tick,
+    onMIDISuccess,
+    onMIDIFailure,
+  };
+};
