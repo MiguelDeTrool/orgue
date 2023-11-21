3c4f4600bb22406f6316da5745045d77e9e184c3
Miguel De Trool <migueldetrool@gmail.com>
2023-11-22 00:48:31 +0100

    Change scan command

--- server/image_scanner.py
+++ server/image_scanner.py
@@ -11,5 +11,9 @@ class ImageScanner:
     img = Image.new("RGB", (32, 32), color="white")
     imgPath = self.path + currTime + ".png"
     img.save(imgPath)
-    # subprocess.run(["scanimage", "--format=png", "--resolution=300", "-x 210", "-y 210", f"--o={self.path}{currTime}.png"])
-    subprocess.run(["scanimage", "--format=png", "--resolution=300", "--mode=24bit Color", "--AutoDocumentSize=yes", f"--o={imgPath}"])
+    subprocess.run(["scanimage", "--format=png", "--resolution=100", "--mode=24bit Color", "-x 210", "-y 210", f"--o={imgPath}"])
+    # subprocess.run(["scanimage", "--format=png", "--resolution=300", "--mode=24bit Color", "--AutoDocumentSize=yes", f"--o={imgPath}"])
+
+# workingPath = "/home/pepite/Pictures/"
+# iS = ImageScanner(workingPath)
+# iS.scanImage()
