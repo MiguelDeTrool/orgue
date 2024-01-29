3a5be0e055a70719bc9993797b7853f89361c18e
Miguel De Trool <migueldetrool@gmail.com>
2024-01-29 14:37:27 +0100

    Add all scan deletion to startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
+rm /home/pepite/orgue/server/static/images/*
 pkill python3
 nohup python3 /home/pepite/orgue/server/app.py &
 sleep 2
 plugdata /home/pepite/orgue/sound/plaits.pd &
 sleep 2
-firefox -kiosk 127.0.0.1:5000
+firefox -kiosk 127.0.0.1:5000 
